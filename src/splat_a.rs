fe8fdf0233c4d19cb732e83cd3481e368826e042
Jonathan Wight <jwight@mac.com>
2024-06-20 20:00:44 -0700

    Support more PLY attributes.

--- src/splat_a.rs
+++ src/splat_a.rs
@@ -14,7 +14,7 @@ pub struct SplatA {
     pub position: Vector3<f32>,       // 3 elements
     pub normal: Option<Vector3<f32>>, // 3 elements (optional)
     pub f_dc: Vector3<f32>,           // 3 elements
-    pub f_rest: Option<[f32; 45]>,    // 45 elements (optional)
+    pub f_rest: Option<Vec<f32>>,     // 45 elements (optional)
     pub opacity: f32,                 // 1 element
     pub scale: Vector3<f32>,          // 3 elements
     pub rot: Vector4<f32>,            // 4 elements
@@ -138,16 +138,14 @@ impl ply_rs::ply::PropertyAccess for SplatA {
             }
         }
 
-        fn update_array(v: Option<[f32; 45]>, index: usize, value: f32) -> [f32; 45] {
+        fn update_array(v: Option<Vec<f32>>, _index: usize, value: f32) -> Vec<f32> {
             match v {
                 Some(mut v) => {
-                    v[index] = value;
+                    v.push(value);
                     v
                 }
                 None => {
-                    let mut v = [0.0; 45];
-                    v[index] = value;
-                    v
+                    vec![value]
                 }
             }
         }
@@ -185,7 +183,7 @@ impl ply_rs::ply::PropertyAccess for SplatA {
                     if index >= 45 {
                         panic!("Vertex: f_rest index out of bounds: {}", index);
                     }
-                    self.f_rest = Some(update_array(self.f_rest, index, v));
+                    self.f_rest = Some(update_array(self.f_rest.clone(), index, v));
                 }
             }
 
@@ -195,12 +193,14 @@ impl ply_rs::ply::PropertyAccess for SplatA {
     }
 }
 
+// SplatScenePoint(position: SIMD3<Float>(2.0078542, 0.08104886, 1.3285047), normal: Optional(SIMD3<Float>(0.0, 0.0, 0.0)), color: SplatIO.SplatScenePoint.Color.sphericalHarmonic(1.1078603, 0.76136017, 0.06554606, [-0.0057055317, -0.007687452, -0.0043917783, -0.0037030454, -0.010418807, 0.06328091, 0.0073155826, -0.0065571507, 0.012642727, 0.006464842, -0.022115033, -0.015164005, 0.03302637, -0.023272093, -0.016852863, 0.006827223, 0.0045320853, -0.005715641, 0.017861754, -0.01044909, 0.0427432, 0.011729195, -0.0037403249, 0.009896112, -0.008849159, -0.00833527, -0.028211955, 0.03504748, 0.014180188, 0.011458171, 0.024590509, 0.01905671, -0.016029052, 0.02723091, -0.025627526, 0.03138132, 0.031709813, 0.0045662196, 0.018008206, -0.022949746, 0.006410549, -0.033924486, 0.029564247, 0.038887653, 0.028849693]), opacity: SplatIO.SplatScenePoint.Opacity.logitFloat(0.84990764), scale: SplatIO.SplatScenePoint.Scale.exponent(-3.8865843, -5.659209, -4.7688828), rotation: simd_quatf(real: 1.0344028, imag: SIMD3<Float>(-0.19919053, -0.10477345, -0.014542822)))
+
 impl From<&SplatA> for UberSplat {
     fn from(splat: &SplatA) -> Self {
         // TODO: FIXME this is all totally invented.
-        let color = Color::LinearFloat(splat.f_dc);
-        let opacity = Opacity::LinearFloat(splat.opacity);
-        let scale = Scale::LinearFloat(splat.scale);
+        let color = Color::SphericalHarmonic(splat.f_dc, splat.f_rest.clone().unwrap());
+        let opacity = Opacity::LogitFloat(splat.opacity);
+        let scale = Scale::Exponent(splat.scale);
         let rotation = Quaternion::new(splat.rot.w, splat.rot.x, splat.rot.y, splat.rot.z);
         UberSplat::new(
             splat.position,
--- src/uber_splat.rs
+++ src/uber_splat.rs
@@ -7,7 +7,7 @@ use tabled::Tabled;
 #[allow(dead_code)]
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub enum Color {
-    SphericalHarmonic(f32, f32, f32, [f32; 15]),
+    SphericalHarmonic(Vector3<f32>, Vec<f32>),
     FirstOrderSphericalHarmonic(Vector3<f32>),
     LinearFloat(Vector3<f32>),
     LinearU8(Vector3<u8>),
@@ -16,7 +16,7 @@ pub enum Color {
 impl Display for Color {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         match self {
-            Color::SphericalHarmonic(_, _, _, _) => write!(f, "SphericalHarmonic"),
+            Color::SphericalHarmonic(_, _) => write!(f, "SphericalHarmonic"),
             Color::FirstOrderSphericalHarmonic(_) => write!(f, "FirstOrderSphericalHarmonic"),
             Color::LinearFloat(v) => write!(f, "LinearFloat({})", v),
             Color::LinearU8(v) => write!(f, "LinearU8({})", v),
@@ -27,9 +27,21 @@ impl Display for Color {
 impl Color {
     pub fn to_linear_float(&self) -> Vector3<f32> {
         match self {
-            Color::SphericalHarmonic(_, _, _, _) => panic!("SphericalHarmonic is not supported"),
-            Color::FirstOrderSphericalHarmonic(_) => {
-                panic!("FirstOrderSphericalHarmonic is not supported")
+            Color::SphericalHarmonic(rgb, _) => {
+                let sh_c0: f32 = 0.282_094_8;
+                Vector3::new(
+                    (0.5 + sh_c0 * rgb[0]).clamp(0.0, 1.0),
+                    (0.5 + sh_c0 * rgb[1]).clamp(0.0, 1.0),
+                    (0.5 + sh_c0 * rgb[2]).clamp(0.0, 1.0),
+                )
+            }
+            Color::FirstOrderSphericalHarmonic(rgb) => {
+                let sh_c0: f32 = 0.282_094_8;
+                Vector3::new(
+                    (0.5 + sh_c0 * rgb[0]).clamp(0.0, 1.0),
+                    (0.5 + sh_c0 * rgb[1]).clamp(0.0, 1.0),
+                    (0.5 + sh_c0 * rgb[2]).clamp(0.0, 1.0),
+                )
             }
             Color::LinearFloat(v) => *v,
             Color::LinearU8(uint8) => Vector3::new(
@@ -41,18 +53,12 @@ impl Color {
     }
 
     pub fn to_linear_u8(&self) -> Vector3<u8> {
-        match self {
-            Color::SphericalHarmonic(_, _, _, _) => panic!("SphericalHarmonic is not supported"),
-            Color::FirstOrderSphericalHarmonic(_) => {
-                panic!("FirstOrderSphericalHarmonic is not supported")
-            }
-            Color::LinearFloat(float) => Vector3::new(
-                (float[0] * 255.0) as u8,
-                (float[1] * 255.0) as u8,
-                (float[2] * 255.0) as u8,
-            ),
-            Color::LinearU8(uint8) => *uint8,
-        }
+        let f = self.to_linear_float();
+        Vector3::new(
+            (f[0] * 255.0) as u8,
+            (f[1] * 255.0) as u8,
+            (f[2] * 255.0) as u8,
+        )
     }
 }
 
@@ -80,19 +86,20 @@ impl Opacity {
     #[allow(dead_code)]
     pub fn to_linear_float(&self) -> f32 {
         match self {
-            Opacity::LinearFloat(float) => *float,
-            Opacity::LinearU8(uint8) => *uint8 as f32 / 255.0,
-            Opacity::LogitFloat(_) => panic!("LogitFloat is not supported"),
+            Opacity::LinearFloat(value) => *value,
+            Opacity::LinearU8(value) => *value as f32 / 255.0,
+            Opacity::LogitFloat(value) => 1.0 / (1.0 + exp(-value)),
         }
     }
 
     pub fn to_linear_u8(&self) -> u8 {
-        match self {
-            Opacity::LinearFloat(float) => (*float * 255.0) as u8,
-            Opacity::LinearU8(uint8) => *uint8,
-            Opacity::LogitFloat(_) => panic!("LogitFloat is not supported"),
-        }
+        let f = self.to_linear_float();
+        (f * 255.0) as u8
     }
+}
+
+fn exp(value: f32) -> f32 {
+    value.exp()
 }
 
 #[allow(dead_code)]
@@ -114,7 +121,7 @@ impl Display for Scale {
 impl Scale {
     pub fn to_linear_float(&self) -> Vector3<f32> {
         match self {
-            Scale::Exponent(_) => panic!("Not supported"),
+            Scale::Exponent(value) => Vector3::new(value[0].exp(), value[1].exp(), value[2].exp()),
             Scale::LinearFloat(v) => *v,
         }
     }
